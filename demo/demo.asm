fd428dba58054091f4708ed6e6646c0cf70c0cb6
bruanth <hugues.bruant@ensimag.imag.fr>
2010-04-09 11:06:15 +0200

    Maybe not...

--- demo/demo.asm
+++ demo/demo.asm
@@ -121,82 +121,84 @@ int_kbd:
 	liw	r2, PS2_rx
 	lw	r2, r2
 	
-	li	r4, 0xF0
-	sub	r4, r2, r4
-	brieq	r4, int_kbd.release
-	
-	li	r4, 0xE0
-	sub	r4, r2, r4
-	brieq	r4, int_kbd.extended
-	
 	mixll	r7, r7, r2
 	
-int_kbd.process:
-	; convert useless scan code representation
-	; into more usable 7bit representation
-	
-	; select proper keymap
-	liw	r4, scan_code_map
-	;shr	r5, r3, 0
-	;shl	r5, r5, 6
-	;add	r4, r4, r5
-	
-	shr	r5, r2, 0
-	sbc	r2, r2, r2
-	
-	add	r4, r4, r5
-	lw	r4, r4
-	
-	brine	r2, int_kbd.process_low
-	mixhh	r2, r2, r4
-	bri	-, int_kbd.processed
-int_kbd.process_low:
-	li	r2, 0
-	mixhl	r2, r2, r4
-	
-int_kbd.processed:
-	
-	
-	; notify
-	
-	; compute address of keybit in keypress_map
-	liw	r4, key_press_map
-	shr	r5, r2, 3
-	add	r4, r4, r5
-	
-	; create bit mask
-	li	r5, 1
-	not	r2, r2
-	rrr	r5, r5, r2
-	
-	lw	r2, r4
-	
-	bspl	r3, r3, 0
-	brine	r3, int_kbd.notify_release
-	
-	; notify keypress
-	or	r2, r2, r5
-	bri	-, int_kbd.notified
-	
-int_kbd.notify_release:
-	not	r5, r5
-	and	r2, r2, r5
-	
-int_kbd.notified:
-	sw	r2, r4
-	
-	; clear status
-	li	r3, 0
-	ba	-, r6
-	
-int_kbd.extended:
-	li	r4, 2
-	or	r3, r3, r4
-	ba	-, r6
-	
-int_kbd.release:
-	li	r4, 1
-	or	r3, r3, r4
+; 	li	r4, 0xF0
+; 	sub	r4, r2, r4
+; 	brieq	r4, int_kbd.release
+; 	
+; 	li	r4, 0xE0
+; 	sub	r4, r2, r4
+; 	brieq	r4, int_kbd.extended
+; 	
+; 	mixll	r7, r7, r2
+; 	
+; int_kbd.process:
+; 	; convert useless scan code representation
+; 	; into more usable 7bit representation
+; 	
+; 	; select proper keymap
+; 	liw	r4, scan_code_map
+; 	;shr	r5, r3, 0
+; 	;shl	r5, r5, 6
+; 	;add	r4, r4, r5
+; 	
+; 	shr	r5, r2, 0
+; 	sbc	r2, r2, r2
+; 	
+; 	add	r4, r4, r5
+; 	lw	r4, r4
+; 	
+; 	brine	r2, int_kbd.process_low
+; 	mixhh	r2, r2, r4
+; 	bri	-, int_kbd.processed
+; int_kbd.process_low:
+; 	li	r2, 0
+; 	mixhl	r2, r2, r4
+; 	
+; int_kbd.processed:
+; 	
+; 	
+; 	; notify
+; 	
+; 	; compute address of keybit in keypress_map
+; 	liw	r4, key_press_map
+; 	shr	r5, r2, 3
+; 	add	r4, r4, r5
+; 	
+; 	; create bit mask
+; 	li	r5, 1
+; 	not	r2, r2
+; 	rrr	r5, r5, r2
+; 	
+; 	lw	r2, r4
+; 	
+; 	bspl	r3, r3, 0
+; 	brine	r3, int_kbd.notify_release
+; 	
+; 	; notify keypress
+; 	or	r2, r2, r5
+; 	bri	-, int_kbd.notified
+; 	
+; int_kbd.notify_release:
+; 	not	r5, r5
+; 	and	r2, r2, r5
+; 	
+; int_kbd.notified:
+; 	sw	r2, r4
+; 	
+; 	; clear status
+; 	li	r3, 0
+; 	ba	-, r6
+; 	
+; int_kbd.extended:
+; 	li	r4, 2
+; 	or	r3, r3, r4
+; 	ba	-, r6
+; 	
+; int_kbd.release:
+; 	li	r4, 1
+; 	or	r3, r3, r4
 	ba	-, r6
 
 
@@ -403,72 +405,72 @@ event_kbd:
 	
 	; interrupt-based conversion, only use key codes
 	
-	liw	r2, key_press_map
-	lw	r2, r2
-	brieq	r2, event_not_kbd
-	
-	; clear previous char
-	li	r3, 0
-	bail	-, r6, putchar
-	; compensate putchar-induced increase of x coordinate
-	dec	r0, r0
-	
-	; up
-	bspl	r3, r2, 1
-	brieq	r3, event_kbd_no_up
-	
-	shr	r3, r1, 2
-	brine	r3, event_kbd_no_clip_up
-	li	r0, 240
-event_kbd_no_clip_up:
-	li	r3, 8
-	sub	r1, r1, r3
-	
-event_kbd_no_up:
-	; left
-	bspl	r3, r2, 2
-	brieq	r3, event_kbd_no_left
-	
-	brine	r0, event_kbd_no_clip_left
-	li	r0, 40
-event_kbd_no_clip_left:
-	dec	r0, r0
-	
-event_kbd_no_left:
-	; down
-	bspl	r3, r2, 3
-	brieq	r3, event_kbd_no_up
-	li	r3, 232
-	sub	r3, r1, r3
-	brilt	r3, event_kbd_no_clip_down
-	li	r0, 0
-event_kbd_no_clip_down:
-	li	r3, 8
-	add	r1, r1, r3
-	
-event_kbd_no_down:
-	; right
-	bspl	r3, r2, 4
-	brieq	r3, event_kbd_no_right
-	li	r3, 39
-	sub	r3, r0, r3
-	brilt	r3, event_kbd_no_clip_right
-	li	r0, -1
-event_kbd_no_clip_right:
-	inc	r0, r0
-	
-event_kbd_no_right:
-
-event_not_kbd:
-	
-	
-	; small delay : ~5M cc ~0.1s
-	li	r4, 20
-	li	r3, 0
-	dec	r3, r3
-	brine	r3, $-1
-	dec	r4, r4
-	brine	r4, $-4
+; 	liw	r2, key_press_map
+; 	lw	r2, r2
+; 	brieq	r2, event_not_kbd
+; 	
+; 	; clear previous char
+; 	li	r3, 0
+; 	bail	-, r6, putchar
+; 	; compensate putchar-induced increase of x coordinate
+; 	dec	r0, r0
+; 	
+; 	; up
+; 	bspl	r3, r2, 1
+; 	brieq	r3, event_kbd_no_up
+; 	
+; 	shr	r3, r1, 2
+; 	brine	r3, event_kbd_no_clip_up
+; 	li	r0, 240
+; event_kbd_no_clip_up:
+; 	li	r3, 8
+; 	sub	r1, r1, r3
+; 	
+; event_kbd_no_up:
+; 	; left
+; 	bspl	r3, r2, 2
+; 	brieq	r3, event_kbd_no_left
+; 	
+; 	brine	r0, event_kbd_no_clip_left
+; 	li	r0, 40
+; event_kbd_no_clip_left:
+; 	dec	r0, r0
+; 	
+; event_kbd_no_left:
+; 	; down
+; 	bspl	r3, r2, 3
+; 	brieq	r3, event_kbd_no_up
+; 	li	r3, 232
+; 	sub	r3, r1, r3
+; 	brilt	r3, event_kbd_no_clip_down
+; 	li	r0, 0
+; event_kbd_no_clip_down:
+; 	li	r3, 8
+; 	add	r1, r1, r3
+; 	
+; event_kbd_no_down:
+; 	; right
+; 	bspl	r3, r2, 4
+; 	brieq	r3, event_kbd_no_right
+; 	li	r3, 39
+; 	sub	r3, r0, r3
+; 	brilt	r3, event_kbd_no_clip_right
+; 	li	r0, -1
+; event_kbd_no_clip_right:
+; 	inc	r0, r0
+; 	
+; event_kbd_no_right:
+; 
+; event_not_kbd:
+; 	
+; 	
+; 	; small delay : ~5M cc ~0.1s
+; 	li	r4, 20
+; 	li	r3, 0
+; 	dec	r3, r3
+; 	brine	r3, $-1
+; 	dec	r4, r4
+; 	brine	r4, $-4
 	
 	bri	-, event_loop
 	
