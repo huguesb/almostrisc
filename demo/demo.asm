39bca476991f66ce29b891b5b1f9ec03cec9c1e2
bruanth <hugues.bruant@ensimag.imag.fr>
2010-04-20 21:30:46 +0200

    Slight reorganization of program data. Made plane sprites masked. Adapted 16*16 sprite routine to use masked sprite and perform basic collision detection. Added failure handling in game loop (both collision detection and boundary check). Added gfx "effect" on failure. Removed old test code from demo.

--- demo/demo.asm
+++ demo/demo.asm
@@ -14,20 +14,20 @@
 
 .equ	font_map		0x12C0
 
-.equ	paper_keys		0x16C0
-.equ	paper_sprites	0x16F0
-.equ	paper_hud		0x1720
-.equ	paper_tiles		0x1730
-.equ	paper_dir		0x173C
-.equ	paper_pos		0x173D
-.equ	paper_speed		0x1740
-.equ	paper_title		0x1744
-.equ	paper_unit		0x174A
-.equ	paper_tilemap	0x1780
+.equ	key_press_map	0x16C0
+.equ	rand_seed		0x16C8
 
-.equ	key_press_map	0x1800
+.equ	paper_keys		0x16D0
+.equ	paper_sprites	0x1700
+.equ	paper_hud		0x1780
+.equ	paper_tiles		0x1790
+.equ	paper_dir		0x179C
+.equ	paper_pos		0x179D
+.equ	paper_speed		0x17A0
+.equ	paper_title		0x17A4
+.equ	paper_unit		0x17AA
+.equ	paper_tilemap	0x17E0
 
-.equ	rand_seed		0x1808
 
 .equ	IRQ_mask		0x2000
 .equ	IRQ_sens		0x2001
@@ -471,16 +471,18 @@ PaperGameTileLoop:
 	shl	r4, r4, 7
 	shr	r4, r4, 7
 	
+	; draw left tile
 	li	r3, 1
 	bailne	r0, r6, put_tile
 	
+	; draw middle tiles
 	li	r3, 2
-	
 PaperGameSegmentLoop:
 	bail	-, r6, put_tile
 	dec	r4, r4
 	brine	r4, PaperGameSegmentLoop
 	
+	; draw right tile
 	li	r3, 0
 	li	r4, 40
 	sub	r4, r0, r4
@@ -495,14 +497,12 @@ PaperGameTileSkip:
 	
 	brilt	r4, PaperGameTileLoop
 	
-	; check for collision
-	
-	
 	; draw plane
 	liw	r3, paper_speed
 	lw	r3, r3
 	li	r4, 0x1F8
 	and	r4, r3, r4
+	add	r4, r4, r4
 	add	r4, r4, r4
 	
 	liw	r3, paper_pos
@@ -514,7 +514,10 @@ PaperGameTileSkip:
 	
 	li	r1, 44
 	li	r3, 16
-	bail	-, r6, put_sprite_16
+	bail	-, r6, put_sprite_16_masked
+	
+	; collision check
+	brine	r5, PaperGameFail
 	
 	; delay
 	li	r0, 5
@@ -543,7 +546,7 @@ PaperGameLoop:
 	lw	r0, r2
 	lw	r1, r3
 	add	r0, r0, r1
-; 	brilt	r0, PaperGameQuit
+	brilt	r0, PaperGameFail
 ; 	li	r4, 300
 ; 	sub	r4, r0, r4
 ; 	brige	r4, PaperGameQuit
@@ -580,23 +583,10 @@ PaperGameScrollLoop:
 	; generate new tilemap line
 	
 ; 	bail	-, r6, rand16
-; 	
-; 	liw	r2, 0x0507
-; 	and	r1, r1, r2
-; 	mixhh	r2, r1, r1
-; 	add	r2, r2, r2
-; 	add	r2, r2, r2
-; 	mixll	r1, r2, r1
-; 	
+	
 	li	r1, 34
 	sw	r1, r0
 	inc	r0, r0
-; 	li	r5, 0
-; 	sw	r5, r0
-; 	inc	r0, r0
-; 	sw	r5, r0
-; 	inc	r0, r0
-; 	sw	r5, r0
 	
 PaperGameSkipScroll:
 	
@@ -641,6 +631,26 @@ PaperNoMoveRIGHT:
 	
 	bai	-, PaperGameRedrawContent
 	
+	
+PaperGameFail:
+	; invert screen
+	; TODO : add color mode(s) to VGA driver???
+	
+	li	r0, 0
+	liw	r2, 240*20
+	
+	lw	r1, r0
+	not	r1, r1
+	sw	r1, r0
+	inc	r0, r0
+	dec	r2, r2
+	brine	r2, $-5
+	
+	; wait for keypress
+	liw	r3, key_press_map
+	
+	lw	r2, r3
+	brieq	r2, $-1
 	
 PaperGameQuit:
 	reset
@@ -723,281 +733,6 @@ put_tile:
 	
 	ba	-, r6
 	
-; 	; old tests
-; 	
-; test.extra:
-; 	; test imm16 loading
-; 	liw	r0, 0x8421
-; 	liw	r1, 0x1234
-; 	
-; 	out	r1
-; 	
-; 	; test reg-mem swap instruction
-; 	exw	r0, r1
-; 	exw	r0, r1
-; 	
-; 	; test mixing instructions
-; 	mixhh	r2, r0, r1
-; 	mixhl	r3, r0, r1
-; 	mixlh	r4, r0, r1
-; 	mixll	r5, r0, r1
-; 	
-; 	; test register-indexed shift/rotates
-; 	li	r6, 3
-; 	
-; 	rrr	r5, r0, r6
-; 	rrl	r5, r1, r6
-; 	rsr	r5, r2, r6
-; 	rsl	r5, r3, r6
-; 	
-; 	; test hw multiplication
-; 	mul	r6, r1, r0 ; r1:r6 = r1 * r0
-; 	
-; test.puts:
-; 	li	r0, 39
-; 	li	r1, 0
-; 	liw	r2, font_map + 4 * 0x23
-; 	li	r3, 8
-; 	bail	-, r6, put_sprite_8_aligned
-; 	
-; 	li	r0, 4
-; 	li	r1, 0
-; 	liw	r2, hello_str
-; 	
-; 	bail	-, r6, puts
-; 	
-; 	
-; 	xor	r0, r0, r0
-; 	li	r1, 20
-; 	
-; 	liw	r2, 0x2020
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0x7070
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0xF8F8 ; 
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0xF8F8 ; 
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0xF870 ; 
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0x7020 ; 
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0x2070 ; 
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	liw	r2, 0x0000 ; 
-; 	sw	r2, r0
-; 	add	r0, r0, r1
-; 	
-; 	; stop there
-; 	;bri	-, $
-; 	
-; 	li	r0, 8
-; 	li	r1, 8
-; 	
-; redraw:
-; 	dec	r7, r7
-; 	sw	r1, r7
-; 	dec	r7, r7
-; 	sw	r0, r7
-; 	
-; 	; display char
-; 	liw	r2, font_map + 4 * 0x23
-; 	li	r3, 8
-; 	bail	-, r6, put_sprite_8
-; 	
-; 	lw	r0, r7
-; 	inc	r7, r7
-; 	lw	r1, r7
-; 	inc	r7, r7
-; 	
-; 	
-; 	; small delay
-; 	li	r2, 20
-; 	li	r3, 0
-; 	dec	r3, r3
-; 	brine	r3, $-1
-; 	dec	r2, r2
-; 	brine	r2, $-4
-; 	
-; event_loop:
-; 	
-; event_kbd:
-; 	
-; 	; keyboard polling...
-; 	
-; ; 	liw	r3, PS2_stat
-; ; 	lw	r3, r3
-; ; 	
-; ; 	bspl	r4, r3, 0
-; ; 	brine	r4, event_not_kbd
-; ; 	
-; ; 	dec	r7, r7
-; ; 	sw	r2, r7
-; ; 	dec	r7, r7
-; ; 	sw	r0, r7
-; ; 	dec	r7, r7
-; ; 	sw	r1, r7
-; ; 	
-; ; 	liw	r3, PS2_rx
-; ; 	lw	r3, r3
-; ; 	
-; ; 	shl	r3, r3, 1
-; ; 	add	r2, r2, r3
-; ; 	
-; ; 	li	r3, 8
-; ; 	bail	-, r6, put_sprite_8_aligned
-; ; 	
-; ; 	lw	r1, r7
-; ; 	inc	r7, r7
-; ; 	lw	r0, r7
-; ; 	inc	r7, r7
-; ; 	
-; ; 	inc	r0, r0
-; ; 	li	r2, 40
-; ; 	sub	r2, r0, r2
-; ; 	brilt	r2, event_kbd_end
-; ; 	shr	r0, r1, 2
-; ; 	li	r2, 8
-; ; 	add	r1, r1, r2
-; ; 	li	r2, 233
-; ; 	sub	r2, r1, r2
-; ; 	brilt	r2, event_kbd_end
-; ; 	li	r1, 8
-; ; 	li	r0, 0
-; ; event_kbd_end:
-; ; 	lw	r2, r7
-; ; 	inc	r7, r7
-; 	
-; 	; interrupt-based conversion, only use key codes
-; 	
-; 	liw	r2, key_press_map
-; 	lw	r2, r2
-; 	brieq	r2, event_not_kbd
-; 	
-; 	dec	r7, r7
-; 	sw	r2, r7
-; 	
-; 	
-; 	dec	r7, r7
-; 	sw	r1, r7
-; 	dec	r7, r7
-; 	sw	r0, r7
-; 	
-; 	; clear char
-; 	liw	r2, font_map
-; 	li	r3, 8
-; 	bail	-, r6, put_sprite_8
-; 	
-; 	lw	r0, r7
-; 	inc	r7, r7
-; 	lw	r1, r7
-; 	inc	r7, r7
-; 	
-; 	
-; 	lw	r2, r7
-; 	inc	r7, r7
-; 	
-; 	; up
-; 	bspl	r3, r2, 0
-; 	brieq	r3, event_kbd_no_up
-; 	
-; 	li	r3, 8
-; 	sub	r3, r1, r3
-; 	brine	r3, event_kbd_no_clip_up
-; 	li	r1, 240
-; event_kbd_no_clip_up:
-; 	li	r3, 8
-; 	sub	r1, r1, r3
-; 	
-; event_kbd_no_up:
-; 	; left
-; 	bspl	r3, r2, 1
-; 	brieq	r3, event_kbd_no_left
-; 	
-; 	brine	r0, event_kbd_no_clip_left
-; 	li	r0, 39*8 ; 40
-; event_kbd_no_clip_left:
-; 	dec	r0, r0
-; 	
-; event_kbd_no_left:
-; 	; down
-; 	bspl	r3, r2, 2
-; 	brieq	r3, event_kbd_no_down
-; 	li	r3, 232
-; 	sub	r3, r1, r3
-; 	brilt	r3, event_kbd_no_clip_down
-; 	li	r1, 0
-; event_kbd_no_clip_down:
-; 	li	r3, 8
-; 	add	r1, r1, r3
-; 	
-; event_kbd_no_down:
-; 	; right
-; 	bspl	r3, r2, 3
-; 	brieq	r3, event_kbd_no_right
-; 	li	r3, 39*8
-; 	sub	r3, r0, r3
-; 	brilt	r3, event_kbd_no_clip_right
-; 	li	r0, -1
-; event_kbd_no_clip_right:
-; 	inc	r0, r0
-; 	
-; event_kbd_no_right:
-; 	bri	-, redraw
-; 	
-; event_not_kbd:
-; 	
-; 	bri	-, event_loop
-	
-	
-	; test division
-test.div:
-	li	r0, 234
-	li	r2, 56
-	bail	-, r6, div_16_16
-	
-	; test multiplication
-test.mult:
-	li	r0, 137
-	li	r2, 142
-	bail	-, r6, mult_16_16
-	
-	; test factorial
-test.factorial:
-	li r2, 7
-	bail	-, r6, fact_16
-	
-	; go back to start
-	reset
-	
-	; r0 : r1 = r0 * r2
-	; destroys : r3, r4, r5
-mult_16_16:
-	xor	r1, r1, r1
-	xor	r4, r4, r4
-	li	r5, 16
-	
-mult_16_16.loop:
-	add	r1, r1, r1	; r1 = r1 * 2
-	adc	r0, r0, r0	; C = msb(r0)
-	sbc	r3, r3, r3	; r3 = sxt(msb(r0))
-	and	r3, r3, r2	; r3 = msb(r0) ? r2 : 0
-	add	r1, r1, r3	; r1 = r1 + r3
-	adc	r0, r0, r4	; carry propagation into r0
-	
-	dec	r5, r5
-	brine	r5, mult_16_16.loop
-	
-mult_16_16.end:
-	ba	-, r6
-	
 
 	; r0 = r0 / r2
 	; r1 = r0 % r2
@@ -1019,27 +754,6 @@ div_16_16.skip:
 	brine	r4, div_16_16.loop
 	
 div_16_16.end:
-	ba	-, r6
-
-
-	; r0 : r1 = r2 !
-	;
-fact_16:
-	xor	r0, r0, r0
-	xor	r1, r1, r1
-	brieq	r2, fact_16.end
-	inc	r1, r1
-fact_16.loop:
-	mova	r0, r1
-	bail	-, r6, mult_16_16
-	
-	brine	r0, fact_16.overflow
-	dec	r2, r2
-	brine	r2, fact_16.loop
-	
-fact_16.overflow:
-	
-fact_16.end:
 	ba	-, r6
 
 	; write a zero-terminated string to screen
@@ -1291,6 +1005,186 @@ put_sprite_16.loop:
 	brine	r3, put_sprite_16.loop
 	
 put_sprite_16.end:
+	
+	ba	-, r6
+
+	; inputs : 
+	;	* (r0, r1) = (x / 16, y)
+	;	* r2 = sprite data (mask interlaced)
+	;	* r3 = sprite height
+	;	
+	;	* r6 = return address 
+	;	
+	; output : 
+	;	* r5 = 0 if no collision
+	;
+	; destroys : r0, r1, r2, r3, r4
+put_sprite_16_masked_aligned:
+	; r4 will be address in VGA buffer : base at 0x0000
+	; buffer is 320*240pix, 16pix per word
+	; => one buffer line is 20 words
+	
+	; r4 = 16*y
+	shl r4, r1, 3
+	; r1 = 4*y
+	shl	r1, r1, 1
+	; r4 = 20*y
+	add	r4, r4, r1
+	; r4 = 20*y + x/16 : first word for sprite
+	add	r4, r4, r0
+	
+	li	r5, 0
+	
+put_sprite_16_masked_aligned.loop:
+	; load buffer value
+	lw	r0, r4
+	
+	; load mask line
+	lw	r1, r2
+	inc	r2, r2
+	
+	; mask buffer
+	and	r0, r0, r1
+	
+	; load sprite line
+	lw	r1, r2
+	inc	r2, r2
+	
+	; compose
+	or	r0, r0, r1
+	
+	; store new value and keep old for collision check
+	exw	r0, r4
+	
+	; collision check
+	and	r0, r0, r1
+	or	r5, r5, r0
+	
+	; next buffer line
+	li	r0, 20
+	add	r4, r4, r0
+	
+	dec	r3, r3
+	brine	r3, put_sprite_16_aligned.loop
+	
+put_sprite_16_masked_aligned.end:
+	
+	ba	-, r6
+
+
+	; inputs : 
+	;	* (r0, r1) = (x, y)
+	;	* r2 = sprite data (mask interlaced)
+	;	* r3 = sprite height
+	;	
+	;	* r6 = return address 
+	;
+	; output : 
+	;	* r5 = 0 if no collision
+	;
+	; destroys : r0, r1, r2, r3, r4
+put_sprite_16_masked:
+	; r4 will be address in VGA buffer : base at 0x0000
+	; buffer is 320*240pix, 16pix per word
+	; => one buffer line is 20 words
+	
+	li	r5, 15
+	and	r5, r0, r5
+	shr	r0, r0, 3
+	
+	brieq	r5, put_sprite_16_masked_aligned
+	dec	r5, r5
+	
+	; r4 = 16*y
+	shl r4, r1, 3
+	; r1 = 4*y
+	shl	r1, r1, 1
+	; r4 = 20*y
+	add	r4, r4, r1
+	; r4 = 20*y + x/16 : first word for sprite
+	add	r4, r4, r0
+	
+	; push r6
+	dec	r7, r7
+	sw	r6, r7
+	
+	mova	r6, r5
+	li	r5, 0
+	
+put_sprite_16_masked.loop:
+	; push r3
+	dec	r7, r7
+	sw	r3, r7
+	
+	; load mask line
+	lw	r0, r2
+	inc	r2, r2
+	
+	; rotate mask appropriately
+	rsr	r0, r0, r6
+	
+	; load sprite line
+	lw	r1, r2
+	
+	; rotate and mask start of sprite line
+	rrr	r1, r1, r6
+	and	r1, r1, r0
+	
+	; compose start of sprite line with first buffer word
+	lw	r3, r4
+	not	r0, r0
+	and	r3, r3, r0
+	or	r3, r3, r1
+	
+	; write new value to buffer but preserve old for collision check
+	exw	r3, r4
+	
+	; collision check
+	and	r3, r3, r1
+	or	r5, r5, r3
+	
+	; move to next buffer word
+	inc	r4, r4
+	
+	; reload sprite line
+	lw	r1, r2
+	
+	; rotate and mask end sprite line
+	rrr	r1, r1, r6
+	and	r1, r1, r0
+	
+	; compose end of sprite line with second buffer word
+	lw	r3, r4
+	not	r0, r0
+	and	r3, r3, r0
+	or	r3, r3, r1
+	
+	; write new value to buffer but preserve old for collision check
+	exw	r3, r4
+	
+	; collision check
+	and	r3, r3, r1
+	or	r5, r5, r3
+	
+	; move to next sprite line
+	inc	r2, r2
+	
+	; move to next buffer line
+	li	r0, 19
+	add	r4, r4, r0
+	
+	; pop	r3
+	lw	r3, r7
+	inc	r7, r7
+	
+	dec	r3, r3
+	brine	r3, put_sprite_16_masked.loop
+	
+put_sprite_16_masked.end:
+	
+	;pop r6
+	lw	r6, r7
+	inc	r7, r7
 	
 	ba	-, r6
 
@@ -1578,45 +1472,6 @@ put_sprite_8.loop1:
 	
 	dec	r3, r3
 	baeq	r3, r6
-	
-; 	li	r2, 0
-; 	mixll	r3, r2, r0
-; 	li	r2, 0xFF
-; 	rsl	r2, r2, r5
-; 	rsl	r3, r3, r5
-; 	
-; 	; load buffer value
-; 	lw	r1, r4
-; 	
-; 	; compose data
-; 	and	r3, r2, r3
-; 	not	r2, r2
-; 	and	r1, r1, r2
-; 	or	r1, r1, r3
-; 	
-; 	; write it to buffer
-; 	sw	r1, r4
-; 	
-; 	inc	r4, r4
-; 	
-; 	li	r2, 0
-; 	mixll	r3, r0, r2
-; 	li	r2, -1
-; 	rsl	r2, r2, r5
-; 	rsl	r3, r3, r5
-; 	
-; 	; load buffer value
-; 	lw	r1, r4
-; 	
-; 	; compose data
-; 	and	r3, r2, r3
-; 	not	r2, r2
-; 	and	r1, r1, r2
-; 	or	r1, r1, r3
-; 	
-; 	; write it to buffer
-; 	sw	r1, r4
-; 	
 	
 	; load sprite data
 	lw	r0, r2
